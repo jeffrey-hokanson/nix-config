fd604aa0da50180df73cb424acca1708c4c65ff0
Jeffrey Hokanson <jeffrey@hokanson.us>
2026-08-01 18:33:35 -0600

    Continuing to debug

--- home.nix
+++ home.nix
@@ -92,7 +92,7 @@
     enableZshIntegration = true;
 
     settings = {
-      format = "$hostname$directory$git_branch$git_status$git_metrics$character";
+      format = "$hostname[](fg:purple bg:blue)$directory[](fg:blue bg:green)$git_branch$git_metrics$character";
 
       hostname = {
         ssh_only = false;
@@ -102,15 +102,15 @@
 
       directory = {
         format = "[ $path ]($style)";
-        style = "fg:blue bg:white";
+        style = "fg:white bg:blue";
         truncation_length = 0;
         truncate_to_repo = false;
       };
 
       git_branch = {
-        format = "[ $symbol$branch(:$remote_branch) ]($style)";
-        symbol = "  ";
-        style = "fg:#030B1 bg:#7DF9AA";
+        format = "[ $symbol $branch ]($style)";
+	symbol = "";
+        style = "fg:white bg:green";
       };
 
       git_status = {
@@ -120,19 +120,9 @@
 
       git_metrics = {
         format = "([+$added]($added_style))[]($added_style)";
-        added_style = "fg:#1C3A5E bg:#FCF392";
-        deleted_style = "fg:bright-red bg:235";
+        added_style = "fg:black bg:yellow";
+        deleted_style = "fg:bright-red bg:black";
         disabled = false;
-      };
-
-      hg_branch = {
-        format = "[ $symbol$branch ]($style)";
-        symbol = " ";
-      };
-
-      cmd_duration = {
-        format = "[  $duration ]($style)";
-        style = "fg:bright-white bg:18";
       };
 
       character = {
